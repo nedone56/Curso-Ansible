b2c5c152df23bcbd3e32b8df229ffabd5a69732a
nedone56 <nedone56@hotmail.com>
2021-07-08 16:21:09 +0200

    Update config.sh

--- vagrant-ansible-controller/config.sh
+++ vagrant-ansible-controller/config.sh
@@ -2,8 +2,13 @@
 
 #Update server
 
-apt-get update
-apt-get upgrade -y 
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible -y
+#apt-get update
+#apt-get upgrade -y 
+#sudo apt-add-repository -y ppa:ansible/ansible
+#sudo apt-get update
+#sudo apt-get install ansible -y
+
+sudo apt-get update -y
+sudo apt-get install build-essential linux-headers-$(uname -r) -y
+sudo apt-get install virtualbox-guest-dkms -y
+

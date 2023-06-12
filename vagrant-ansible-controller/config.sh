4af26d71676f4f995959b112d543101ed639dd2b
nedone56 <nedone56@hotmail.com>
2023-06-12 12:49:58 +0200

    Update config.sh

--- vagrant-ansible-controller/config.sh
+++ vagrant-ansible-controller/config.sh
@@ -12,3 +12,14 @@ sudo apt-get update -y
 sudo apt-get install build-essential linux-headers-$(uname -r) -y
 sudo apt-get install virtualbox-guest-dkms -y
 
+sleep 10
+
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible -y
+
+
+sleep 5
+
+sudo sed -i 's/prohibit-password/yes/g' /etc/ssh/sshd_config
+

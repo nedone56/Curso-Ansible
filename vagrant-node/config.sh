725ef9e92caafa91d62a4be17351821f14521c37
nedone56 <nedone56@hotmail.com>
2023-06-12 12:50:40 +0200

    Update config.sh

--- vagrant-node/config.sh
+++ vagrant-node/config.sh
@@ -3,3 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install build-essential linux-headers-$(uname -r) -y
 sudo apt-get install virtualbox-guest-dkms -y
+
+sleep 5
+
+sudo sed -i 's/prohibit-password/yes/g' /etc/ssh/sshd_config
